54ed6215c8b1e314ec5eb7959400bffb69b18123
hennychen <junheng.chen@pmwinner.com>
2024-01-29 10:39:43 +0800

    <p>签名区域</p>SignaturePad

--- v7ionicapp/src/app/app.module.ts
+++ v7ionicapp/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { SignaturePadModule } from '@drkmtrx/angular2-signaturepad';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+ 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,SignaturePadModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- v7ionicapp/src/app/tab3/tab3.page.ts
+++ v7ionicapp/src/app/tab3/tab3.page.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SignaturePad } from '@drkmtrx/angular2-signaturepad';
 import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+  @ViewChild(SignaturePad)
+  signaturePad!: SignaturePad;
+
+  signaturePadOptions = {
+    // passed through to szimek/signature_pad constructor
+    minWidth: 5,
+    canvasWidth: 500,
+    canvasHeight: 300,
+  };
 
   constructor(public photoService: PhotoService) {}
+  ngAfterViewInit() {
+    // this.signaturePad is now available
+    this.signaturePad.set('minWidth', 5); // set szimek/signature_pad options at runtime
+    this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
+  }
 
-  pick(){
+  drawComplete() {
+    // will be notified of szimek/signature_pad's onEnd event
+    console.log(this.signaturePad.toDataURL());
+  }
+
+  drawStart() {
+    // will be notified of szimek/signature_pad's onBegin event
+    console.log('begin drawing');
+  }
+  pick() {
     this.photoService.pickPhoto(); // 调用PhotoService的pickPhoto方法
   }
 }
